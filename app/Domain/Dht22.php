b6d00ca584c28ce317534d956c91bef0509989ff
Bret Hash <bret@brethash.com>
2016-08-27 22:32:27 -0600

    Briefly using sudo to debug

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -11,7 +11,7 @@ class DHT22 extends Gpio
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
-        exec('loldht', $output, $return_var);
+        exec('sudo loldht', $output, $return_var);
         return $output;
     }
 
