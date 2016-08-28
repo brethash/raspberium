611bb86b4ec1b26cc90e117fddbb2a22df62173c
Bret Hash <bret@brethash.com>
2016-08-27 22:34:21 -0600

    Removing sudo

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -11,7 +11,7 @@ class DHT22 extends Gpio
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
-        exec('sudo loldht', $output, $return_var);
+        exec('loldht', $output, $return_var);
         return $output;
     }
 
