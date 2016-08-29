6928cc3644fa9768102406f2a160a51a7ac1710b
Bret Hash <bret@brethash.com>
2016-08-28 22:09:15 -0600

    Finally fixed the dht22 sensor. Issue stemmed from system user perms

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -11,8 +11,10 @@ class DHT22 extends Gpio
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
-        exec('loldht', $output, $return_var);
-        return $output;
+        exec('sudo /usr/bin/loldht', $output, $return_var);
+
+        // Return the last element in the array as this is the only one with usable data.
+        return array_pop($output);
     }
 
 }
