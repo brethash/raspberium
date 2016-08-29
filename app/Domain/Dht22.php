d1511cf4eef8e0174719605355d28254640a7e0a
Bret Hash <bret@brethash.com>
2016-08-28 23:00:01 -0600

    Adding temperature and humidity breakout functions

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -7,12 +7,33 @@ use Raspberium\Contracts\Gpio;
 class DHT22 extends Gpio
 {
     
-    
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
         exec('loldht', $output, $return_var);
         return $output;
+    }
+
+    public function getHumidity()
+    {
+        $splitValues = $this->getSplitValues();
+        $humidity = $splitValues[0];
+        $humidityArray = explode('=',$humidity);
+        return trim($humidityArray[1]);
+    }
+
+    public function getTemperature()
+    {
+        $splitValues = $this->getSplitValues();
+        $temperature = $splitValues[0];
+        $temperatureArray = explode('=',$temperature);
+        return trim($temperatureArray[1]);
+    }
+
+    private function getSplitValues()
+    {
+        $output = $this->read();
+        return explode('%',$output);
     }
 
 }
--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -12,10 +12,21 @@ class SensorController extends Controller
         return false;
     }
 
+    public function getTemperature()
+    {
+        $dht22 = new DHT22($this->getDht22Pin());
+        echo $dht22->getTemperature();
+    }
+
+    public function getHumidity()
+    {
+        $dht22 = new DHT22($this->getDht22Pin());
+        echo $dht22->getHumidity();
+    }
+
     public function readDht22()
     {
         $dht22 = new DHT22($this->getDht22Pin());
-        echo "test";
         echo "<pre>";
         var_dump($dht22->read());
         echo "</pre>";
