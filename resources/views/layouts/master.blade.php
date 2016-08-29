557bc2cce090e5672af8478c7d90dde2ec692e8a
Bret Hash <bret@brethash.com>
2016-08-29 17:08:46 -0600

    Updating footer copyright and version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -72,10 +72,9 @@
 
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 2.3.6
+            <b>Version</b> 1.0.0
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-        reserved.
+        <strong>Copyright &copy;2016 <a href="http://brethash.com">Bret Hash</a>.</strong> All rights reserved.
     </footer>
 
     <!-- Control Sidebar -->
