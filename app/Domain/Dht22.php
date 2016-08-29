64ccc599aa942aef451c49a13db647b377d6b5d2
Bret Hash <bret@brethash.com>
2016-08-28 23:01:29 -0600

    Refixing the dht22 exec call

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -10,7 +10,7 @@ class DHT22 extends Gpio
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
-        exec('loldht', $output, $return_var);
+        exec('sudo /usr/bin/loldht', $output, $return_var);
         return $output;
     }
 
