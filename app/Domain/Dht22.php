fea9bc10704508c76cdaa1a083e464d31496286d
Bret Hash <bret@brethash.com>
2016-08-29 11:11:04 -0600

    Fixed up Dht22 class with comments and fixed getTemperature method to return temp instead of humidity

--- app/Domain/Dht22.php
+++ app/Domain/Dht22.php
@@ -6,7 +6,12 @@ use Raspberium\Contracts\Gpio;
 
 class DHT22 extends Gpio
 {
-    
+
+    /**
+     * Reads the output from the loldht script
+     *
+     * @return array
+     */
     public function read() {
         // usage: loldht <pin> (<tries>)
         $return_var = 0;
@@ -14,6 +19,11 @@ class DHT22 extends Gpio
         return $output;
     }
 
+    /**
+     * Parses the humidity into a double from the getSplitValues string
+     *
+     * @return string
+     */
     public function getHumidity()
     {
         $splitValues = $this->getSplitValues();
@@ -22,20 +32,37 @@ class DHT22 extends Gpio
         return trim($humidityArray[1]);
     }
 
+    /**
+     * Parses the temperature into a double from the getSplitValues string
+     *
+     * @return mixed
+     */
     public function getTemperature()
     {
         $splitValues = $this->getSplitValues();
-        $temperature = $splitValues[0];
+        $temperature = $splitValues[1];
         $temperatureArray = explode('=',$temperature);
-        return trim($temperatureArray[1]);
+        return trim(str_replace('*C','',$temperatureArray[1]));
     }
 
+    /**
+     * Splits the read value from the loldht script for parsing
+     *
+     * @return string
+     */
     private function getSplitValues()
     {
-        $output = $this->read();
+        // TODO: check if readValue is a valid array
+        $readValue = $this->read();
+        $output = array_pop($readValue);
         return explode('%',$output);
     }
 
+    /**
+     * Gets the configued DHT22 pin number
+     *
+     * @return integer
+     */
     public static function getDht22Pin() {
         return env('DHT22_PIN', false);
     }
