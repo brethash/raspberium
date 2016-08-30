d8e613c71927da7b1670cb7ea694e7766f9685a5
Bret Hash <bret@brethash.com>
2016-08-29 18:01:11 -0600

    Moving titles into blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.master')
+    @section('title', 'Raspberium - Home')
     @section('content')
         <!-- Content Header (Page header) -->
         <section class="content-header">
