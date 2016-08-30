b32a90c6e48a4658dfc3e5d8b40b621d38a8282a
Bret Hash <bret@brethash.com>
2016-08-29 20:16:14 -0600

    Fixing gauge diplay issue

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,24 +1,26 @@
+var temperatureOptions = {
+    width: 400, height: 200,
+    yellowFrom: 0, yellowTo: 20,
+    greenFrom: 20, greenTo: 30,
+    redFrom: 30, redTo: 50,
+    minorTicks: 5,
+    min: 0,
+    max: 50
+};
+
+var humidityOptions = {
+    width: 400, height: 200,
+    redFrom: 10, redTo: 30,
+    yellowFrom: 30, yellowTo: 55,
+    greenFrom: 55, greenTo: 100,
+    minorTicks: 5,
+    min: 10,
+    max: 100
+};
+
 $(function(){
 
-    var temperatureOptions = {
-        width: 400, height: 200,
-        yellowFrom: 0, yellowTo: 20,
-        greenFrom: 20, greenTo: 30,
-        redFrom: 30, redTo: 50,
-        minorTicks: 5,
-        min: 0,
-        max: 50
-    };
 
-    var humidityOptions = {
-        width: 400, height: 200,
-        redFrom: 10, redTo: 30,
-        yellowFrom: 30, yellowTo: 55,
-        greenFrom: 55, greenTo: 100,
-        minorTicks: 5,
-        min: 10,
-        max: 100
-    };
 
 
     updateGauge('humidityGauge','Humidity',30, humidityOptions);
