bc37f01beb00aa37edbda1d9a4e1ecc27a37fcde
Bret Hash <bret@brethash.com>
2016-08-30 18:06:57 -0600

    Fixing up light controller. Much less garbage

--- app/Http/Controllers/LightController.php
+++ app/Http/Controllers/LightController.php
@@ -7,61 +7,27 @@ use Raspberium\Domain\Relay;
 class LightController extends Controller
 {
 
-    public function index()
-    {
-        return false;
-    }
-
     public function light1On()
     {
-        $light = new Relay($this->getLight1Pin());
-        try {
-            $light->on();
-        }
-        catch (\Exception $e) {
-            echo 'false';
-        }
-
-        echo 'flerp';
+        $light = new Relay(Relay::getLight1Pin());
+        echo $light->on();
     }
 
     public function light1Off()
     {
-        $light = new Relay($this->getLight1Pin());
-        try {
-            $light->off();
-        }
-        catch (\Exception $e) {
-            echo 'false';
-        }
-
-        echo 'flerp';
+        $light = new Relay(Relay::getLight1Pin());
+        echo $light->off();
     }
 
     public function light2On()
     {
-        $light = new Relay($this->getLight2Pin());
-        try {
-            $light->on();
-        }
-        catch (\Exception $e) {
-            echo 'false';
-        }
-
-        echo 'flerp';
+        $light = new Relay(Relay::getLight2Pin());
+        echo $light->on();
     }
 
     public function light2Off()
     {
-        $light = new Relay($this->getLight2Pin());
-        try {
-            $light->off();
-        }
-        catch (\Exception $e) {
-            echo 'false';
-        }
-
-        echo 'flerp';
+        $light = new Relay(Relay::getLight2Pin());
+        echo $light->off();
     }
-
 }
