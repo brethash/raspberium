d5d72b4e9ffa471513a8b70386e1f1771f5afbab
Bret Hash <bret@brethash.com>
2016-09-06 21:51:13 -0600

    Added authentication. Such wow.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,31 @@
 
 namespace Raspberium\Providers;
 
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Raspberium\Models\Configuration;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
+     * Sharing is caring
      *
      * @return void
      */
     public function boot()
     {
-        //
+        // TODO: Implement view composer???
+        // Share user data to view
+//        var_dump(Auth::user()->toArray());
+//       View::share('user',User::all('name')->first()->toArray());
+//
+//        var_dump(User::all('name')->first()->toArray());
+
+        // Share configuration data to view
+        View::share(Configuration::getData());
     }
 
     /**
--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -7,7 +7,7 @@
                 <img src="/img/user2-160x160.jpg" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Bret Hash</p>
+                <p>{{ Auth::user()->name }}</p>
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
