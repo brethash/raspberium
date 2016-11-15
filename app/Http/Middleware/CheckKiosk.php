ad9be2fa7d82dae22106a3be8ac683ed397223e1
Bret Hash <bret@brethash.com>
2016-11-14 19:41:17 -0700

    CheckKiosk middleware finished.

--- app/Http/Middleware/CheckKiosk.php
+++ app/Http/Middleware/CheckKiosk.php
@@ -3,6 +3,7 @@
 namespace Raspberium\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 
 class CheckKiosk
@@ -16,11 +17,45 @@ class CheckKiosk
      */
     public function handle($request, Closure $next)
     {
-        if ($request->kiosk == "true")
+        $key = "kiosk";
+        $kioskMode = false;
+
+        // Get all request params
+        $params = $request->all();
+
+        // If there are no request params found but the kiosk session key exists, we're still in kiosk mode
+        if (!count($params) && Session::has($key))
         {
-            View::share(array('kiosk' => true));
+            // If our session variable is true, set kioskMode to true
+            $sessionKiosk = Session::get($key);
+            if ($sessionKiosk == "true")
+            {
+                $kioskMode = true;
+            }
+
         }
 
+        // If there are request params
+        if (count($params))
+        {
+            // And one of them happens to be kiosk and its true
+            if ($request->kiosk == "true")
+            {
+                // Setup kiosk mode!
+                Session::put($key, "true");
+            }
+            else if ($request->kiosk == "false")
+            {
+                // We no longer want kiosk mode enabled
+                Session::put($key, "false");
+            }
+
+        }
+
+        if ($kioskMode) {
+            View::share(array('kiosk' => true));
+        }
+        
         return $next($request);
     }
 }
