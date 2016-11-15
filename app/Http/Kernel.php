48806397d7e855bacfcdba6d408a2923a330a68a
Bret Hash <bret@brethash.com>
2016-11-15 16:04:43 -0700

    Removing global config from global middleware stack

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Raspberium\Http\Middleware\GlobalConfiguration::class
     ];
 
     /**
