89ac3dbb0bd6ef06ba8a9b68cb42a69656ab6491
Bret Hash <bret@brethash.com>
2016-11-21 14:45:17 -0700

    Adding state classes to actions buttons

--- resources/views/actions.blade.php
+++ resources/views/actions.blade.php
@@ -25,7 +25,7 @@
                         <ul>
                             <li class="btn-group">
                                 <button class="btn btn-default
-                                        @if ($light1['status'] == 'on')
+                                        @if ($light1['state'] == 'on')
                                         btn-primary
                                         @endif
                                         "
@@ -34,15 +34,55 @@
                                         data-state="on"
 
                                 >Turn Light 1 On</button>
-                                <button class="btn btn-default" id="light1Timer" data-device="light1" data-state="timer">Timer</button>
-                                <button class="btn btn-default" id="light1Off" data-device="light1" data-state="off">Turn Light 1 Off</button>
+                                <button class="btn btn-default
+                                        @if ($light1['state'] == 'timer')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="light1Timer"
+                                        data-device="light1"
+                                        data-state="timer"
+                                >Timer</button>
+                                <button class="btn btn-default
+                                        @if ($light1['state'] == 'off')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="light1Off"
+                                        data-device="light1"
+                                        data-state="off"
+                                >Turn Light 1 Off</button>
                             </li>
                         </ul>
                         <ul>
                             <li class="btn-group">
-                                <button class="btn btn-default" id="light2On" data-device="light2" data-state="on">Turn Light 2 On</button>
-                                <button class="btn btn-default" id="light2Timer" data-device="light2" data-state="timer">Timer</button>
-                                <button class="btn btn-default" id="light2Off" data-device="light2" data-state="off">Turn Light 2 Off</button>
+                                <button class="btn btn-default
+                                        @if ($light2['state'] == 'on')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="light2On"
+                                        data-device="light2"
+                                        data-state="on"
+                                >Turn Light 2 On</button>
+                                <button class="btn btn-default
+                                        @if ($light2['state'] == 'timer')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="light2Timer"
+                                        data-device="light2"
+                                        data-state="timer"
+                                >Timer</button>
+                                <button class="btn btn-default
+                                        @if ($light2['state'] == 'off')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="light2Off"
+                                        data-device="light2"
+                                        data-state="off"
+                                >Turn Light 2 Off</button>
                             </li>
                         </ul>
                     </div>
@@ -50,9 +90,33 @@
                         <h3>Misting System Control</h3>
                         <ul>
                             <li class="btn-group">
-                                <button class="btn btn-default" id="pump1On" data-device="mistingSystem1" data-state="on">Turn Pump On</button>
-                                <button class="btn btn-default" id="pump1Timer" data-device="mistingSystem1" data-state="timer">Timer</button>
-                                <button class="btn btn-default" id="pump1Off" data-device="mistingSystem1" data-state="off">Turn Pump Off</button>
+                                <button class="btn btn-default
+                                        @if ($pump1['state'] == 'on')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="pump1On"
+                                        data-device="mistingSystem1"
+                                        data-state="on"
+                                >Turn Pump On</button>
+                                <button class="btn btn-default
+                                        @if ($pump1['state'] == 'timer')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="pump1Timer"
+                                        data-device="mistingSystem1"
+                                        data-state="timer"
+                                >Timer</button>
+                                <button class="btn btn-default
+                                        @if ($pump1['state'] == 'off')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="pump1Off"
+                                        data-device="mistingSystem1"
+                                        data-state="off"
+                                >Turn Pump Off</button>
                             </li>
                         </ul>
                     </div>
@@ -60,9 +124,33 @@
                         <h3>Fan System Control</h3>
                         <ul>
                             <li class="btn-group">
-                                <button class="btn btn-default" id="fan1On" data-device="fan1" data-state="on">Turn Fan On</button>
-                                <button class="btn btn-default" id="fan1Timer" data-device="fan1" data-state="timer">Timer</button>
-                                <button class="btn btn-default" id="fan1Off" data-device="fan1" data-state="off">Turn Fan Off</button>
+                                <button class="btn btn-default
+                                        @if ($fan1['state'] == 'on')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="fan1On"
+                                        data-device="fan1"
+                                        data-state="on"
+                                >Turn Fan On</button>
+                                <button class="btn btn-default
+                                        @if ($fan1['state'] == 'timer')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="fan1Timer"
+                                        data-device="fan1"
+                                        data-state="timer"
+                                >Timer</button>
+                                <button class="btn btn-default
+                                        @if ($fan1['state'] == 'off')
+                                        btn-primary
+                                        @endif
+                                        "
+                                        id="fan1Off"
+                                        data-device="fan1"
+                                        data-state="off"
+                                >Turn Fan Off</button>
                             </li>
                         </ul>
                     </div>
