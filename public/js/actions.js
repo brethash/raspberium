c203a4c611f90043d887036ddf8be6a34b8ba44b
Bret Hash <bret@brethash.com>
2016-11-21 17:52:53 -0700

    Removing old console.log

--- public/js/actions.js
+++ public/js/actions.js
@@ -4,11 +4,10 @@ $(function(){
         $this = $(this);
         $alertMessage = $('.alert-box h2');
         $.get({
-            url: '/relay/' + $this.data('device') + '/' + $this.data('state'),
+            url: '/device/' + $this.data('device') + '/' + $this.data('state'),
             success: function(data){
                 $alertMessage.addClass('success').removeClass('failure').slideDown(500).html(data);
                 $this.parent('.btn-group').children().each(function(){
-                    console.log($(this).attr('id'))
                     $(this).removeClass('btn-primary');
                 });
                 $this.addClass('btn-primary');
