38f25e64b188b562edaffb2e52352d5a7e006372
Bret Hash <bret@brethash.com>
2017-02-23 13:28:57 -0700

    Adding new python resources to get individual values

---
+++ resources/bme280/humidity.py
@@ -0,0 +1,7 @@
+from Adafruit_BME280 import *
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+humidity = sensor.read_humidity()
+
+print 'Humidity  = {0:0.2f} %'.format(humidity)
---
+++ resources/bme280/pressure.py
@@ -0,0 +1,8 @@
+from Adafruit_BME280 import *
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+pascals = sensor.read_pressure()
+hectopascals = pascals / 100
+
+print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
