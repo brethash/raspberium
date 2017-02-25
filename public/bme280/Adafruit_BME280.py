71a847ce2ff854b677424ceba608491729342987
Bret Hash <bret@brethash.com>
2017-02-25 00:31:32 -0700

    python things

--- public/bme280/Adafruit_BME280.py
+++ public/bme280/Adafruit_BME280.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
 #
--- public/bme280/pressure.py
+++ public/bme280/pressure.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from Adafruit_BME280 import *
 
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
