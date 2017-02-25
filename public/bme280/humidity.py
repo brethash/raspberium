7cad331f6b46373b15df7c45b1b41ac17e02e7eb
Bret Hash <bret@brethash.com>
2017-02-25 14:07:20 -0700

    Accounting for weird bug where you have to read the temperature first...

--- public/bme280/humidity.py
+++ public/bme280/humidity.py
@@ -4,6 +4,8 @@ from Adafruit_BME280 import *
 
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
+# for some reason we have to read the temperature first. i'm guessing this is a bug.
+degrees = sensor.read_temperature()
 humidity = sensor.read_humidity()
 
-print 'Humidity  = {0:0.2f} %'.format(humidity)
+print '{0:0.2f}'.format(humidity)
--- public/bme280/pressure.py
+++ public/bme280/pressure.py
@@ -4,7 +4,9 @@ from Adafruit_BME280 import *
 
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
+# for some reason we have to read the temperature first. i'm guessing this is a bug.
+degrees = sensor.read_temperature()
 pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 
-print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+print '{0:0.2f}'.format(hectopascals)
